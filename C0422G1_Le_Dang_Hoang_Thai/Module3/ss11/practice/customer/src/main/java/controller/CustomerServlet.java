06b3ec416ade9d20bd14c9a406505046d015119f
thaildh95 <thaildh.rtbed@gmail.com>
2022-07-29 09:37:30 +0700

    up

---
+++ C0422G1_Le_Dang_Hoang_Thai/Module3/ss11/practice/customer/src/main/java/controller/CustomerServlet.java
@@ -0,0 +1,71 @@
+package controller;
+
+import model.Customer;
+import service.CustomerService;
+import service.impl.CustomerServiceImpl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "CustomerServlet", value = "/CustomerServlet")
+
+public class CustomerServlet extends HttpServlet {
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Customer> customers = this.customerService.findAll();
+        request.setAttribute("customers", customers);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CustomerService customerService = new CustomerServiceImpl();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:    listCustomers(request, response);
+                break;
+        }
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            default:
+                break;
+        }
+    }
+    }
+
---
+++ C0422G1_Le_Dang_Hoang_Thai/Module3/ss11/practice/customer/src/main/webapp/customer/customer_list.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 7/29/2022
+  Time: 9:16 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Danh sách khách hàng</title>
+</head>
+<body>
+
+</body>
+</html>
